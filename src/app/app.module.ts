bbeda79a504037f07d71e7bbaa889e57f392bb6b
faiyaz <faiyaz@provility.com>
2022-10-14 10:06:12 +0530

    hero component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroComponent,
+    NavbarComponent,
+    DescriptionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
